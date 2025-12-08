5119753dd58dd5e55657fb8da62d89dd9db602e9
vishdevs <vishalshende0101@gmail.com>
2025-12-08 16:53:32 +0530

    Delete backend directory

--- backend/src/main/java/com/inventory/repository/Sales0rderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inventory.repository;
-
-import com.inventory.model.SalesOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesOrderRepository extends JpaRepository<SalesOrder, Long> {
-}
--- backend/src/main/java/com/inventory/repository/SalesItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inventory.repository;
-
-import com.inventory.model.SalesItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesItemRepository extends JpaRepository<SalesItem, Long> {
-}
